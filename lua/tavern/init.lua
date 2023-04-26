548e33d4d89a65163effde41d8d9279fcfdfb58b
MysteryMage <36642316+MysteryMage@users.noreply.github.com>
2023-04-26 10:27:38 +0300

    chore: stylua and formatting.

--- lua/tavern/init.lua
+++ lua/tavern/init.lua
@@ -1,13 +1,13 @@
 local M = {}
 
-local theme = require("tavern.theme")
+local theme = require('tavern.theme')
 
 function M.setup()
-	require("tavern.util.highlighter").apply_theme(theme.get_theme())
+    require('tavern.util.highlighter').apply_theme(theme.get_theme())
 
-	if vim.g.tavern_transparent == true then
-		vim.api.nvim_set_hl(0, "Normal", {})
-	end
+    if vim.g.tavern_transparent == true then
+        vim.api.nvim_set_hl(0, 'Normal', {})
+    end
 end
 
 return M
